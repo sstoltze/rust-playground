16077ceeb1a5e5ae3ca7c1888c002a6a44573dc4
sstoltze <sstoltze@gmail.com>
2019-11-09 01:37:16 +0100

    Implement building an index from a file

--- file-search/src/main.rs
+++ file-search/src/main.rs
@@ -1,28 +1,44 @@
 use std::collections::HashMap;
-use std::fs::read_dir;
-use std::io::Result;
+use std::fs::{read_dir, File};
+use std::io::{BufRead, BufReader, Result};
 use std::path::{Path, PathBuf};
 
+#[derive(Debug)]
 struct WordPosition {
     line: u32,
     column: u32,
 }
 
+#[derive(Debug)]
 struct IndexEntry {
     file: PathBuf,
-    positions: Vec<WordPosition>,
+    position: WordPosition,
     containing_line: String,
 }
 
-fn build_index(file: PathBuf) -> HashMap<String, Vec<IndexEntry>> {
-    let _ie = IndexEntry {
-        file,
-        positions: Vec::new(),
-        containing_line: "test".to_string(),
-    };
-    let mut h = HashMap::new();
-    h.insert("test".to_string(), Vec::new());
-    h
+type WordIndex = HashMap<String, Vec<IndexEntry>>;
+
+fn build_index(file: PathBuf) -> Result<WordIndex> {
+    let mut file_index: WordIndex = HashMap::new();
+
+    let f = File::open(file.clone())?;
+    let reader = BufReader::new(f);
+
+    for (index, line) in (0..).zip(reader.lines()) {
+        let line = line?;
+        for word in line.split_whitespace() {
+            let ie = IndexEntry {
+                file: file.clone(),
+                position: WordPosition {
+                    line: index + 1,
+                    column: 0,
+                },
+                containing_line: line.clone(),
+            };
+            file_index.entry(String::from(word)).or_default().push(ie);
+        }
+    }
+    Ok(file_index)
 }
 
 fn find_files(p: PathBuf) -> Result<Vec<PathBuf>> {
@@ -33,8 +49,7 @@ fn find_files(p: PathBuf) -> Result<Vec<PathBuf>> {
             if !file_str.starts_with(".") {
                 if p.is_dir() {
                     for entry in read_dir(p)? {
-                        let entry = entry?;
-                        let path = entry.path();
+                        let path = entry?.path();
                         let mut vec = find_files(path)?;
                         res.append(&mut vec);
                     }
@@ -48,8 +63,15 @@ fn find_files(p: PathBuf) -> Result<Vec<PathBuf>> {
 }
 
 fn main() -> Result<()> {
-    let p = std::fs::canonicalize(Path::new("../.."))?;
+    let p = std::fs::canonicalize(Path::new("."))?;
     let files = find_files(p)?;
     println!("{:?}", files);
+    for f in files.iter() {
+        let index = build_index(f.clone())?;
+        println!("{:?}", index);
+        for ie in index["column:"].iter() {
+            println!("--- {} ---", ie.containing_line);
+        }
+    }
     Ok(())
 }
