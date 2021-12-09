e3ef736c4627e33686a1ced8d05f3ff944a168c6
Sarah Stoltze <sstoltze@gmail.com>
2021-12-09 13:01:44 +0100

    Linting/reformatting

--- jenkins/src/main.rs
+++ jenkins/src/main.rs
@@ -7,16 +7,13 @@ use serde::Deserialize;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-#[derive(Deserialize)]
-#[serde(rename_all = "camelCase")]
 struct BuildInfo {
     #[allow(dead_code)] // We currently don't use the job_name
     job_name: String,
-    build: Option<CommonBuild>,
+    last_build: Option<CommonBuild>,
 }
 
 #[derive(Deserialize)]
-#[serde(rename_all = "camelCase")]
 struct JenkinsJob {
     name: String,
     url: String,
@@ -27,39 +24,15 @@ struct JenkinsJobs {
     jobs: Vec<JenkinsJob>,
 }
 
-fn job_branches(jenkins: &Jenkins, job: &str) -> Result<Vec<JenkinsJob>> {
-    let jobs: JenkinsJobs = jenkins.get_object_as(
-        Path::Job {
-            name: job,
-            configuration: None,
-        },
-        TreeBuilder::new()
-            .with_field(
-                TreeBuilder::object("jobs")
-                    .with_subfield("name")
-                    .with_subfield("url"),
-            )
-            .build(),
-    )?;
-    Ok(jobs.jobs)
-}
-
-fn build_info(job: &JenkinsJob, branch: &str) -> Result<BuildInfo> {
-    // We want the branch at $JENKINS_URL/job/<job_name>/job/<branch_name>,
-    // but get_object_as does not support getting sub-objects (using name: <job_name>/job/<branch_name> sanitises the / to %2F which does not work).
-    // So we need to build a new root
-    let jenkins = JenkinsBuilder::new(&job.url).build()?;
-    let job = jenkins.get_job(branch)?;
-    let build_info = match job.last_build {
-        Some(b) => {
-            let info = b.get_full_build(&jenkins)?;
-            Some(info)
-        }
+fn build_info(jenkins: &Jenkins, branch: &JenkinsJob) -> Result<BuildInfo> {
+    let job = jenkins.get_job(&branch.name)?;
+    let last_build = match job.last_build {
+        Some(b) => Some(b.get_full_build(jenkins)?),
         None => None,
     };
     Ok(BuildInfo {
         job_name: job.name,
-        build: build_info,
+        last_build,
     })
 }
 
@@ -85,22 +58,28 @@ fn main() -> Result<()> {
     let jobs = get_jobs(&jenkins)?;
 
     for job in jobs {
-        println!("{}", job.name);
-        let branches = job_branches(&jenkins, &job.name)?;
-        println!("  Branches",);
+        // We want the branch at $JENKINS_URL/job/<job_name>/job/<branch_name>,
+        // but get_object_as does not support getting sub-objects (using name: <job_name>/job/<branch_name> sanitises the / to %2F which does not work).
+        // So we need to build a new root for getting branches
+        let job_jenkins = JenkinsBuilder::new(&job.url).build()?;
+        let branches = get_jobs(&job_jenkins)?;
+        println!("{} - {} branches.", job.name, branches.len());
+
         for branch in branches {
-            println!("    {}", branch.name);
-            let info = build_info(&job, &branch.name)?;
-            match info.build {
-                None => println!("      No build info"),
-                Some(info) => println!(
-                    "      Last build: {}, {}",
+            println!("  {}", branch.name);
+
+            let info = build_info(&job_jenkins, &branch)?;
+            let build_info = match info.last_build {
+                None => "No build info".to_string(),
+                Some(info) => format!(
+                    "Last build: {}, {}",
                     info.result
                         .map(|b| format!("{:?}", b))
                         .unwrap_or_else(|| "In progress".to_string()),
                     format_timestamp(info.timestamp)
                 ),
-            }
+            };
+            println!("    {}", build_info);
         }
     }
 
